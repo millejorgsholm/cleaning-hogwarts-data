0577838a858be6cc902dbff92685245769804c67
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-14 17:33:54 +0100

    images added

--- script.js
+++ script.js
@@ -135,9 +135,9 @@ function displayStudent(student) {
   clone.querySelector("[data-field=nickname]").textContent = student.nickName;
   clone.querySelector("[data-field=gender]").textContent = student.gender;
   clone.querySelector("[data-field=house]").textContent = student.house;
-  // clone.querySelector("img").src = `/images/${
-  //   student.lastName
-  // }_${student.firstName.charAt(0)}.png`;
+  clone.querySelector("img").src = `/images/${
+    student.lastName
+  }_${student.firstName.charAt(0)}.png`;
 
   //append clone to list
   document.querySelector("#list tbody").appendChild(clone);
